9a705e5246028d9514176c3400bbd41da92e5026
Sagar Shrivastava <sagar_shrivastava_sateschnlogies@whirlpool.com>
2026-04-15 13:17:42 +0530

    Testing git pipeline

--- stock-screening/app.py
+++ stock-screening/app.py
@@ -147,7 +147,11 @@ with tab_macro:
 # ==========================================
 with tab1:
     st.header("Step 1: Fundamental Screening")
-
+    i: int = 0
+    j: int = 0
+    qwer
+    while True:
+        i=i+1
 
     # 1. Grab the AI-generated query if it exists
     default_fund = st.session_state.get("auto_fundamental", "")
